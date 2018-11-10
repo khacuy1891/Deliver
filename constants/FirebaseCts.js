7f11f0f4bf69748964794558d329a869401a578a
unknown <uybkt2@gmail.com>
2018-11-11 01:20:21 +0700

    Add FirebaseCts.js file

---
+++ constants/FirebaseCts.js
@@ -0,0 +1,27 @@
+export default {
+    KEY_TOKEN: "xxx",
+
+    config: {
+        apiKey: "xxx",
+        authDomain: "xxx",
+        databaseURL: "xxx",
+        projectId: "xxx",
+        storageBucket: "xxx",
+        messagingSenderId: "xxx",
+    },
+
+    //Table Delivers
+    TABLE_DELIVERS: "delivers",
+    DELIVER_WORKSTATUS: "workstatus",
+    DELIVER_LAT: "lat",
+    DELIVER_LNG: "lng",
+    DELIVER_LAST_UPDATE: "last_update",
+    DELIVER_FULLNAME: "fullname",
+    DELIVER_ADDRESS: "address",
+
+    //Table Orders
+    TABLE_ORDERS: "orders",
+    ORDER_ACCEPTTIME: "AcceptTime",
+    ORDER_STATUS: "orderstatus",
+    ORDER_INFO_FEEDBACK: "infofeedback",
+};
